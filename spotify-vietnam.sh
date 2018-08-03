5c0cc48723598d3d4179c4fed28cf8d3db4dd972
paczor <38018844+bitounu@users.noreply.github.com>
2018-08-03 07:54:13 +0200

    datails

--- spotify-vietnam.sh
+++ spotify-vietnam.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
 # check if credentials file is in same folder
+# if yes, read it
 if [ ! -f ./creds ]; then
         echo "Ceredentials not found!"
 else
         . ./creds
 fi
 
-PLAYLIST="Vietnam Soundtrack"
+PLAYLIST_NAME="Vietnam Soundtrack"
+# make playlist public
+PLAYLIST_PUBLIC="true"
+
 
 # --------------------------------
 # ADD PLAYLIST
-#curl -X POST "https://api.spotify.com/v1/users/$USER/playlists" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" --data "{\"name\":\"$PLAYLIST\", \"public\":false}"
+PLAYLIST_ID=`curl -s -X POST "https://api.spotify.com/v1/users/$USER/playlists" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" --data "{\"name\":\"$PLAYLIST_NAME\", \"public\":$PLAYLIST_PUBLIC}" | grep -m1 id | cut -d: -f2 | tr -d [:punct:]`
 
-PLAYLIST_ID=""
 # --------------------------------
 # ADD SONG TO PLAYLIST
 # Parameters:
 # uris: 
 #   coma separated list of songs in following format: spotify:track:id,spotify:track:id
 #   colon in url is iencoded as %3A
-#   nie wiecej niz 100 piosenek w jednym wywolaniu
-#   najlepiej wiec wywolac po stworzeniu listy piosenek
+#   no more than 100 songs in one request
+#   better call once when list of songs is ready
 # position
-    pozycja na ktorej ma sie pojawic piosenka na playliście
-    jeśli parametr jest pominiety
+#   position for the song on playlist
+#   song is APPENDED if this parameter is ommitted
 
-#curl -X "POST" "https://api.spotify.com/v1/users/$USER/playlists/$PLAYLIST_ID/tracks?position=$i&uris=spotify%3Atrack%3A4iV5W9uYEdYUVa79Axb7Rh%2Cspotify%3Atrack%3A1301WleyT98MSxVHPZCA6M" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN"
+echo "PLAYLIST_NAME: $PLAYLIST_NAME"
+echo "PLAYLIST_ID: $PLAYLIST_ID"
+
+curl -s -X "POST" "https://api.spotify.com/v1/users/$USER/playlists/$PLAYLIST_ID/tracks?uris=spotify%3Atrack%3A5FQ7JGp1moW918V80hUGfK" -H "Content-Length: 0" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN"
